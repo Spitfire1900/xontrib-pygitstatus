c3463cad172eb6410b5abdc99d8c6703c7ae45e9
Kyle Gottfried <6462596+Spitfire1900@users.noreply.github.com>
2024-03-22 23:13:21 -0400

    add SetEnv.ps1

---
+++ SetEnv.ps1
@@ -0,0 +1,4 @@
+$Env:XDG_CACHE_HOME="$(Get-Location)/.xdg/.cache"
+$Env:XDG_CONFIG_HOME="$(Get-Location)/.xdg/.config"
+$Env:XDG_DATA_HOME="$(Get-Location)/.xdg/.local/share"
+$Env:XONSHRC="$(Get-Location)/.xonshrc"
