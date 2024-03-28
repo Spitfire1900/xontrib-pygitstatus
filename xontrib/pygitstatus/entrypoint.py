3c149adcf02b8751aa03fb6a782baf9735a23863
Kyle Gottfried <6462596+Spitfire1900@users.noreply.github.com>
2024-03-27 21:00:33 -0400

    implement _unload_xontrib_

--- xontrib/pygitstatus/entrypoint.py
+++ xontrib/pygitstatus/entrypoint.py
@@ -5,7 +5,18 @@ from xonsh.built_ins import XonshSession
 from .prompts import (ahead, behind, branch, changed, clean, repo_path, short_head, stash_count, tag, untracked)
 
 
-def _load_xontrib_(xsh: XonshSession, **_):
+def _load_xontrib_(xsh: XonshSession, **_) -> dict:
+    """
+    this function will be called when loading/reloading the xontrib.
+
+    Args:
+        xsh: the current xonsh session instance, serves as the interface to manipulate the session.
+             This allows you to register new aliases, history backends, event listeners ...
+        **kwargs: it is empty as of now. Kept for future proofing.
+    Returns:
+        dict: this will get loaded into the current execution context
+    """
+
     # getting environment variable
     var = xsh.env.get('VAR', 'default')
 
@@ -27,8 +38,25 @@ def _load_xontrib_(xsh: XonshSession, **_):
     prompt_fields['pygitstatus.stash_count'] = stash_count
     prompt_fields['pygitstatus.tag'] = tag
     prompt_fields['pygitstatus.untracked'] = untracked
+    return {}
 
 
 def _unload_xontrib_(xsh: XonshSession, **kwargs) -> dict:
-    """If you want your extension to be unloadable, put that logic here"""
-    raise NotImplementedError('unload of pygitstatus not implemented')
+    """
+    If you want your extension to be unloadable, put that logic here
+
+    Args:
+        xsh: the current xonsh session instance, serves as the interface to manipulate the session.
+             This allows you to register new aliases, history backends, event listeners ...
+        **kwargs: it is empty as of now. Kept for future proofing.
+    Returns:
+        dict: this will get loaded into the current execution context
+    """
+
+    prompt_fields: MutableMapping
+    prompt_fields = xsh.env.get('PROMPT_FIELDS')  # type: ignore
+    key: str
+    for key in list(prompt_fields.keys()):
+        if key.startswith('pygitstatus'):
+            del prompt_fields[key]
+    return {}
