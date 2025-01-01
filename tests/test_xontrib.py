4472e352671758e4b4a4ed2c54199d88c3461ca4
Kyle Gottfried <6462596+Spitfire1900@users.noreply.github.com>
2025-01-01 10:24:25 -0500

    actually it's conflicted files

--- tests/test_xontrib.py
+++ tests/test_xontrib.py
@@ -401,7 +401,7 @@ def test_gitstatus(git_repo):
 
         print('> git status')
         print(git_repo.git.status())
-        # BUG?: gitstatus does not inlcude deleted files
+        # BUG?: gitstatus does not include conflicted files
         # assert PromptFormatter()('{pygitstatus}') == PromptFormatter()('{gitstatus}')
         expected = '{CYAN}f1↑·1↓·1{CYAN}|MERGING{RESET}|{RED}●3{RESET}{RED}×1{RESET}{BLUE}+1{RESET}{RED}-1{RESET}…1⚑1'
         assert PromptFormatter()('{pygitstatus}') == expected
